a99a2992ac6e42b928e22cf1ceae6580478023e6
thiagotlse <paulo.francisco@gmail.com>
2019-07-31 16:11:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-editor-wysiwyg-fecrbh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-editor-wysiwyg-fecrbh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular 6';
+  htmlContent = '';
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    toolbarPosition: 'top',
+    customClasses: [
+      {
+        name: "quote",
+        class: "quote",
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: "titleText",
+        class: "titleText",
+        tag: "h1",
+      },
+    ]
+  };
+}
